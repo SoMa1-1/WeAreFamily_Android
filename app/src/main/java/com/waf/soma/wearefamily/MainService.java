ea0d5ad61ff5c321744dcd5f7022396ca48a57e5
tigersden <expect017@naver.com>
2015-08-09 02:59:25 +0900

    GPS서비스, 락 이벤트 전송 구현

--- app/src/main/java/com/waf/soma/wearefamily/MainService.java
+++ app/src/main/java/com/waf/soma/wearefamily/MainService.java
@@ -3,9 +3,11 @@ package com.waf.soma.wearefamily;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
@@ -14,15 +16,71 @@ import android.widget.Toast;
 
 public class MainService extends Service {
 
+    LocationManager mLocMan;
+    String mProvider;
+    String TAG="TEST";
+    int mCount;
 
-    LocationManager manager;
-
-    public void onCreate(){
+    @Override
+    public void onCreate() {
         super.onCreate();
-        Log.i("sv","service started");
 
-        
+        Log.i(TAG, "onCreate 호출");
+        mLocMan = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mProvider = mLocMan.getBestProvider(new Criteria(),true);
+
+        //원래 OnResume에 있던 코드
+        mLocMan.requestLocationUpdates(mProvider, 3000, 10, mListener);
     }
+
+    /*
+    public void onResume(){
+        super.onResume();
+        mCount=0;
+        //
+        Log.i(TAG,"현재 상태 : 서비스 시작");
+    }
+
+    public void onPause(){
+        super.onPause();
+        mLocMan.removeUpdates(mListener);
+        Log.i(TAG,"현재 상태 : 서비스 정지");
+    }
+    */
+
+    LocationListener mListener = new LocationListener(){
+        public void onLocationChanged(Location location){
+            mCount++;
+            String sloc = String.format("수신회수:%d 위도:%f 경도:%f 고도:%f",
+                    mCount,location.getLatitude(),location.getLongitude(),location.getAltitude());
+            Log.i(TAG,sloc);
+
+            //new HttpTask().execute();
+        }
+        public void onProviderDisabled(String provider){
+            Log.i(TAG,"현재 상태 : 서비스 사용 불가");
+        }
+
+        public void onProviderEnabled(String provider){
+            Log.i(TAG,"현재 상태 : 서비스 사용 가능");
+        }
+
+        public void onStatusChanged(String provider,int status,Bundle extras){
+            String sStatus="";
+            switch(status){
+                case LocationProvider.OUT_OF_SERVICE:
+                    sStatus="범위 벗어남";
+                    break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    sStatus="범위 벗어남";
+                    break;
+                case LocationProvider.AVAILABLE:
+                    sStatus="사용 가능";
+                    break;
+            }
+            Log.i(TAG, provider+"상태 변경 : "+sStatus);
+        }
+    };
 
     @Override
     public IBinder onBind(Intent intent) {
